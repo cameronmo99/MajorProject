38d4ac3bbaf1813585bb3d4e36d81ffd55088da0
Cameronmo99 <cameronmo99@gmail.com>
2019-02-13 20:14:39 +0100

    Initial Player Controller (Need Work)

---
+++ Major Project/Assets/Scripts/Player/PlayerControllerSinglePlayer.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControllerSinglePlayer : MonoBehaviour
+{
+    public float PlayerRotationSpeed;
+    public float Speed;
+    public GameObject Player;
+    public Rigidbody PlayerRigidBody;
+
+
+    void Update()
+    {
+        //Player Rotation
+        Player.transform.Rotate(0, PlayerRotationSpeed * Input.GetAxis("Horizontal"), 0);
+    }
+
+    void FixedUpdate()
+    {
+        //Player Movement
+        PlayerRigidBody.AddForce((transform.forward * Speed) * Input.GetAxis("Vertical"));
+    }
+}
